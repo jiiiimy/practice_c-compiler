34edb9b187da1339e097ea1bca331089b6201fff
Ryota Yajima <ryotayajima@Ryotas-MBP.lan>
2023-01-02 15:44:15 +0900

    add: tokenizer

--- cc.c
+++ cc.c
@@ -1,5 +1,110 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// Types of token
+typedef enum
+{
+    TK_RESERVED, // Sign
+    TK_NUM,      // Number
+    TK_EOF,      // End of file
+} TokenKind;
+
+typedef struct Token Token;
+// Token Type
+struct Token
+{
+    TokenKind kind; // Type of token
+    Token *next;    // Next token
+    int val;        // Number, when kind is TK_NUM
+    char *str;      // String of token
+};
+// Current feature token
+Token *token;
+
+void error(char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+bool consume(char op)
+{
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+        return false;
+    token = token->next;
+    return true;
+}
+
+void expect(char op)
+{
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+        error("'%c'ではありません", op);
+    token = token->next;
+}
+
+int expect_number()
+{
+    if (token->kind != TK_NUM)
+        error("数値ではありません");
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof()
+{
+    return token->kind == TK_EOF;
+}
+
+Token *new_token(TokenKind kind, Token *cur, char *str)
+{
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
+}
+
+Token *tokenize(char *p)
+{
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while (*p)
+    {
+        if (isspace(*p))
+        {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-')
+        {
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+
+        if (isdigit(*p))
+        {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        error("トークナイズできません");
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
+}
 
 int main(int argc, char **argv)
 {
@@ -9,30 +114,28 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    token = tokenize(argv[1]);
+
     char *p = argv[1];
 
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov x0, %ld\n", strtol(p, &p, 10));
+    printf("  mov x0, %ld\n", expect_number());
 
-    while (*p)
+    while (!at_eof())
     {
-        if (*p == '+')
+        if (consume('+'))
         {
-            p++;
-            printf("  add x0, x0, %ld\n", strtol(p, &p, 10));
-            continue;
+            printf("  add x0, x0, %d\n", expect_number());
         }
 
-        if (*p == '-')
+        if (consume('-'))
         {
-            p++;
-            printf("  sub x0, x0, %ld\n", strtol(p, &p, 10));
-            continue;
+            printf("  sub x0, x0, %d\n", expect_number());
         }
 
-        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-        return 1;
+        printf("  ret\n");
+        return 0;
     }
 
     printf("  ret\n");
